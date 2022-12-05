4977e9db728fb48f0efd1bc98f0579ba2f1d4be4
Reed Meher <reed.meher@gmail.com>
2022-12-05 11:52:58 -0600

    refactoring savedBooks part 2

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,40 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
-import { getMe, deleteBook } from '../utils/API';
+//mutation and query from apollo
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+ 
+  // refactored useEffect to useQuery hook
+  const { loading, error, userData } = useQuery(GET_ME); 
 
-  // use this to determine if `useEffect()` hook needs to run again
+  // is define books necessary?
+  const books = userData?.savedBooks || [];
+
   const userDataLength = Object.keys(userData).length;
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {

dc8c485af41a6500416f10138a39c28e9cced13c
Reed Meher <reed.meher@gmail.com>
2022-12-07 11:35:46 -0600

    removed requires from savedBooks

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 //mutation and query from apollo
 import { useMutation, useQuery } from '@apollo/client';
